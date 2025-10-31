42335e954b2560e58b0de206f68566892808497a
vikramroybarc <vikramroy2050@gmail.com>
2025-10-31 19:22:22 +0530

    DeleteEverything

--- examples/bcc_FeCr/IrradCreep/64g/673K/runmoose2.sh
+++
@@ -1,5 +0,0 @@
-
-cd 350MPa
-mpirun -n 24 ~/projects/rhocp-vr/rhocp-opt -i bcc_pxtal.i sigmaV=350 Executioner/TimeStepper/log_dt=0.010 Executioner/end_time=1.0e3 Outputs/file_base=out_ICreep_673K_350MPa
-mpirun -n 24 ~/projects/rhocp-vr/rhocp-opt -i bcc_pxtal.i sigmaV=350 Executioner/TimeStepper/log_dt=0.001 Executioner/end_time=1.0e5 Outputs/file_base=out_ICreep_673K_350MPa --recover
-mpirun -n 24 ~/projects/rhocp-vr/rhocp-opt -i bcc_pxtal.i sigmaV=350 Executioner/TimeStepper/log_dt=0.001 Executioner/end_time=2.5e7 Outputs/file_base=out_ICreep_673K_350MPa --recover
